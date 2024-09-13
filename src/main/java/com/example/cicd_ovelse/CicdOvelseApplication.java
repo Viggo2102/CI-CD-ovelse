1e55c02fc8c6f82b59ed27dc5d1e42da17f3099e
Viggo2102 <146332112+Viggo2102@users.noreply.github.com>
2024-09-13 12:56:09 +0200

    nnj

---
+++ src/main/java/com/example/cicd_ovelse/CicdOvelseApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd_ovelse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdOvelseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdOvelseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicd_ovelse/CicdOvelseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_ovelse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdOvelseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
